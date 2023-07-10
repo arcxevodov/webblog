5d399e1232a7156abb1b470dacc022e2e92edc13
Артур <artur.archevodov@gmail.com>
2023-07-10 19:13:54 +0300

    CRUD: 1.1 - index для категорий

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -14,7 +14,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="{{ route('categories') }}" class="nav-link">
                     <i class="nav-icon fas fa-th-list"></i>
                     <p>Категории</p>
                 </a>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,10 @@ Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
 
 Route::group(['namespace' => '\App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
     Route::group(['namespace' => 'Main'], function () {
-        Route::get('/', 'IndexController');
+        Route::get('/', 'IndexController')->name('admin');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController')->name('categories');
     });
 });
 
