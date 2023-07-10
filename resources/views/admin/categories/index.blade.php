69cae1514f24dad4db57f252288f2a562b6a3b33
Артур <artur.archevodov@gmail.com>
2023-07-10 19:20:00 +0300

    Косетические улучшения

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -6,12 +6,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">{{ config('app.name') }} | Категории</h1>
                     </div>
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item"><a href="{{ route('admin') }}">Панель управления</a></li>
+                            <li class="breadcrumb-item active">Категории</li>
                         </ol>
                     </div>
                 </div>
