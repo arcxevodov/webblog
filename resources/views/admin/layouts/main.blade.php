3fc6e71d646fc3d9bfed1443e08a3a212cf05603
Артур <artur.archevodov@gmail.com>
2023-07-10 18:39:19 +0300

    Небольшая чистка кода админки

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>{{ config('app.name') }} | Admin Dashboard</title>
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
@@ -12,6 +13,7 @@
     <link rel="stylesheet" href="{{ asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/daterangepicker/daterangepicker.css') }}">
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
         <div class="preloader flex-column justify-content-center align-items-center">
@@ -24,20 +26,11 @@
                 </li>
             </ul>
         </nav>
-        <!-- /.navbar -->
 
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-
-        </aside>
+        @include('admin.includes.sidebar')
 
         @yield('content')
-
-        <footer class="main-footer">
-
-        </footer>
     </div>
-    <!-- ./wrapper -->
-
     <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
     <script>
@@ -49,4 +42,5 @@
     <script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <script src="{{ asset('dist/js/adminlte.js') }}"></script>
 </body>
+
 </html>
